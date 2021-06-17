ff78fd56d872ece4f4048b67abf0fc93e511d1df
miguel soto <miguelsr.1084@gmail.com>
2021-06-16 23:02:08 -0500

    atributos de revicion texnica completadas

--- model/client.go
+++ model/client.go
@@ -1,21 +1,22 @@
 package model
 
 type Client struct {
-	ID             uint   `gorm:"primaryKey" json:"id"` // ,omitempty
-	Name           string `gorm:"type:varchar(100); default:''; not null" json:"name"`
-	LastName       string `gorm:"type:varchar(100); default:''; not null" json:"last_name"`
-	Email          string `gorm:"uniqueIndex; type:varchar(255); not null" json:"email"`
-	Password       string `gorm:"type:varchar(255); not null" json:"password"`
-	RememberToken  string `gorm:"type:varchar(250)" json:"remember_token"`
-	Dni            string `gorm:"uniqueIndex; type:char(8)" json:"dni"`
-	Ruc            string `gorm:"type:varchar(40); default:''" json:"ruc"`
-	Phone          string `gorm:"type:varchar(20); default:''" json:"phone"`
-	Picture        string `gorm:"type:varchar(250)" json:"picture"`
-	Address        string `gorm:"type:varchar(250); default:''" json:"address"`
-	State          string `gorm:"type:varchar(25); not null" json:"state"`
-	OrderAttention string `gorm:"type:char(2);default:0; not null" json:"order_attention"`
-	Uri            string `gorm:"uniqueIndex; type:varchar(250)" json:"uri"`
-	RoleID         uint8  `json:"role_id"`
-	Appointments   []Appointment
+	ID               uint   `gorm:"primaryKey" json:"id"` // ,omitempty
+	Name             string `gorm:"type:varchar(100); default:''; not null" json:"name"`
+	LastName         string `gorm:"type:varchar(100); default:''; not null" json:"last_name"`
+	Email            string `gorm:"uniqueIndex; type:varchar(255); not null" json:"email"`
+	Password         string `gorm:"type:varchar(255); not null" json:"password"`
+	RememberToken    string `gorm:"type:varchar(250)" json:"remember_token"`
+	Dni              string `gorm:"uniqueIndex; type:char(8)" json:"dni"`
+	Ruc              string `gorm:"type:varchar(40); default:''" json:"ruc"`
+	Phone            string `gorm:"type:varchar(20); default:''" json:"phone"`
+	Picture          string `gorm:"type:varchar(250)" json:"picture"`
+	Address          string `gorm:"type:varchar(250); default:''" json:"address"`
+	State            string `gorm:"type:varchar(25); not null" json:"state"`
+	OrderAttention   string `gorm:"type:char(2);default:0; not null" json:"order_attention"`
+	Uri              string `gorm:"uniqueIndex; type:varchar(250)" json:"uri"`
+	RoleID           uint8  `json:"role_id"`
+	Appointments     []Appointment
+	TechnicalReviews []TechnicalReview
 	TimeModel
 }
---
+++ model/technicalReview.go
@@ -0,0 +1,17 @@
+package model
+
+type TechnicalReview struct {
+	ID                uint   `gorm:"primaryKey" json:"id"` // ,omitempty
+	CarFeatures       string `gorm:"type:varchar(350); default:''" json:"car_features"`
+	CommonFaults      string `gorm:"type:varchar(250); default:''" json:"common_faults"`
+	TechnicalAnalysis string `gorm:"type:varchar(350); default:''" json:"technical_analysis"`
+	VehicleType       string `gorm:"type:varchar(30); default:''" json:"vehicle_type"`
+	RevisionType      bool   `gorm:"default:false" json:"revision_type"`
+	Requirements      string `gorm:"type:varchar(350); default:''" json:"requirements"`
+	ServicesVehicle   string `gorm:"type:varchar(50); default:''" json:"services_vehicle"`
+	PickUp            bool   `gorm:"default:false; not null" json:"pick_up"`
+	Arrangements      string `gorm:"type:varchar(350); default:''" json:"arrangements"`
+	VehicleCondition  bool   `gorm:"default:false; not null" json:"vehicle_condition"`
+	ClientID          uint   `gorm:"not null" json:"client_id"`
+	TimeModel
+}
